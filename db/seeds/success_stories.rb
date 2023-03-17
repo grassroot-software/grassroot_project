dcd8d271a9299fee18d2bd82a7131b75027288a3
yayahc <hamedcuenca5@gmail.com>
2023-03-17 16:42:28 +0000

    satble v7 --add pic

--- db/seeds/success_stories.rb
+++ db/seeds/success_stories.rb
@@ -4,21 +4,21 @@ success_stories = [
     avatar_path_name: 'aron_fischer.jpg',
     story_content: "<p>I remember when I first tried to learn to code. It's not an accident that I say I tried because if I'm honest, I started a few times before finally sticking to it. I tried many different resources, courses, and curriculums, but none of them seemed to teach in a way that made me stick to it.</p> </p> What differentiates Grassroot from all those other courses and curriculums out there is its unique way of providing guidance and answers to the most important questions, while also strengthening your ability to push through the curriculum and projects by yourself and as a result grow on your struggles.</p> <p>The reason why Grassroot was perfect for me was on the one hand because in its' well-structured paths I found answers to the most difficult of all questions: Where should I start? What should I learn first? How well do I have to know certain technologies? Besides providing answers to those questions, having a great and supportive community, another point to stress is the flexibility of the curriculum.</p> <p>I was able to use my creativity to design and code the projects, and often further ideas were provided to help you figure out which feature to add next. Now, I reached what I wouldn't have thought possible when I started. I landed a job :D.</p>",
     job_title: 'Reactjs Frontend Developer',
-    social_media_link: 'https://github.com/aronfischer'
+    social_media_link: '#'
   },
   {
     student_name: 'Melisa',
-    avatar_path_name: 'Melisa.jpg',
+    avatar_path_name: 'me.jpg',
     story_content: '<p>just 5 months of part-time study, I went from "It s too tough for me" to "with enough time, I can build anything with code". That confidence boost alone is enough evidence for me to say that Grassroot learning model is by far the best I have ever experienced. After completing the Full curriculum, I applied to one dream job, and I got it! It was the best investment of my time that I made. Even better than going to university!</p>',
     job_title: 'Full Stack specialist at BlueChip Solutions - Nairobi, Kenya',
-    social_media_link: ''
+    social_media_link: '#'
   },
   {
-    student_name: 'Rob Pando',
+    student_name: 'George Leo',
     avatar_path_name: 'rob_pando.jpg',
     story_content: '<p>Grassroot was a perfect match for my learning style. Not walking me through an entire project but actually forcing me to figure it out on my own, by pointing me in all the right directions in order to complete them. That helped me dig deeper into various important topics, which then gave me the necessary knowledge and confidence to start applying for jobs and landing that dream job. The community was part of it too, first getting help and then helping, it was a great way to learn and make it stick.</p>',
     job_title: 'Fullstack Developer',
-    social_media_link: 'https://github.com/RobPando'
+    social_media_link: '#'
   },
   {
     student_name: 'Olivia',
@@ -32,7 +32,7 @@ success_stories = [
     avatar_path_name: 'cody_loyd.jpg',
     story_content: '<p>Before I started The Grassroot Project I had literally ZERO programming experience. After almost exactly a year of working through the program, I was offered a job as a front end web developer.</p> <p>Grassroot taught me all the most crucial skills in webdev and gave me an incredibly strong foundation for branching out and learning other pieces of the web-dev puzzle along the way.</p>',
     job_title: 'Software Engineer',
-    social_media_link: 'http://codyloyd.com/'
+    social_media_link: '#'
   },
   {
     student_name: 'William Kennedy',
@@ -42,32 +42,32 @@ success_stories = [
     social_media_link: '#'
   },
   {
-    student_name: 'Andrej Dragojević',
+    student_name: 'Jackson Andrej',
     avatar_path_name: 'andrej_dragojevic.jpg',
     story_content: '<p>An incredible self-paced curriculum that consists of the best resources for learning programming on the web! It was an invaluable resource on my path to a becoming a software developer.</p> <p>Thanks to The Grassroot Project I was able to get a job half way through the curriculum, and the projects completed as part of the curriculum gave me an edge compared to other junior developers with no experience.</p>',
     job_title: 'Full Stack Ruby on Rails Developer',
-    social_media_link: 'https://www.linkedin.com/in/andrej-dragojevic-735a51b7/'
+    social_media_link: '#'
   },
   {
-    student_name: 'Skip Gibson',
+    student_name: 'Gibson Yapo',
     avatar_path_name: 'skip.jpg',
     story_content: "<p>Before I found The Grassroot Project I had very little direction with my learning, jumping from topic to topic then trying to piece things together as I went.</p> <p>Having this online curriculum to follow was a massive help and took my understanding of programming to the next level. Being self taught and going into a job interview with no professional experience is incredibly daunting, but being able to point to my GitHub which had all of the projects I'd completed while going through the curriculum, I was able to demonstrate that I was committed to learning and improving myself, ultimately that's exactly what they were after in an employee.</p> <p>If you're considering using The Grassroot Project I really cannot recommend it enough, I owe my career to this site.</p>",
     job_title: 'Software Developer',
-    social_media_link: 'https://github.com/skipcloud'
+    social_media_link: '#'
   },
   {
-    student_name: 'Mazin Power Elfehaid',
+    student_name: 'Mazin Eli',
     avatar_path_name: 'mazin.jpg',
     story_content: "<p>In 2014 I wanted to learn how to program, but I had no idea where to start. Grassroot solved that problem for me - it mapped out a series of lesson plans that showed me what to study, without holding my hand or spoon-feeding me information.</p> <p>The curriculum requires hard work and perseverance, but it's worth it. I walked into my first job and felt instantly comfortable with the tools and technologies my employer required. Yes, there was a lot that I didn't know yet, but having done Grassroot also gave me the confidence to pick up new skills as needed.</p>",
     job_title: 'Software Engineer',
-    social_media_link: 'https://github.com/muzfuz'
+    social_media_link: '#'
   },
   {
-    student_name: 'Sean Szurko',
+    student_name: 'Neas Alban',
     avatar_path_name: 'sean.jpg',
     story_content: '<p>Grassroot smoothed out the steep learning curve in web development, allowing me to confidently learn at my own pace and taught me how to approach and solve real-life programming problems.</p> <p>The projects in Grassroot were a perfect mix of difficulty, allowing me to gain exposure to different technologies and feel challenged without feeling overwhelmed. Because of the foundation Grassroot provided, I was able to easily transition into freelancing and then into a junior level Rails developer position.</p>',
     job_title: 'Mid-Level Rails Developer',
-    social_media_link: 'https://www.seanszurko.com/',
+    social_media_link: '#',
   }  
 ]
 
