895de53a83041ec89a51b05e9971b0a34bb519e3
yayahc <hamedcuenca5@gmail.com>
2023-03-18 22:57:58 +0000

    try remove odin

--- config/initializers/odin_configuration.rb
+++ config/initializers/odin_configuration.rb
@@ -1,2 +1,2 @@
-ODIN_BLOG_URL = 'https://dev.to/theodinproject'.freeze
-ODIN_CHAT_URL = 'https://discord.gg/fbFCkYabZB'.freeze
+ODIN_BLOG_URL = 'https://dev.to/'.freeze
+ODIN_CHAT_URL = 'https://discord.gg/tnztvakSka'.freeze
