3dafd09c1aae28f071a5b4baa182c15ace45f309
yayahc <hamedcuenca5@gmail.com>
2023-04-26 14:29:59 +0000

    redirect path changed#3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || dashboard_path
+    stored_location_for(resource) || root_path
   end
 
   def not_found_error
